ad44493c263848a4bb6b24d5079ac18d699390b0
jp378 <jpoutoa865@gmail.com>
2025-09-26 03:40:56 +1200

    Changed file names, modified arduino programs AND python scripts. modified sensor python script to print milliseconds in he timestamp.

--- sensorData_to_haptics.py
+++ sensorData_to_haptics.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import serial
+import time
 
-
-COM_PORT = 'COM7'   #Adjust if needed
+COM_PORT = 'COM6'   #Adjust if needed
 BAUD_RATE = 115200
 CSV_FILE = "sync_test2509.csv"
 VIDEO_FILE = "sync_test2509.mp4"
@@ -15,3 +16,20 @@ def mag_to_strength(mag):
         return "Med"
     else:
         return "Low"
+
+
+try:
+    with serial.Serial(COM_PORT, BAUD_RATE, timeout=1) as ser:
+        print(f"Connected to {COM_PORT}")
+        print("Type 'exit' or 'quit' to end")
+        while True:
+            user_input = input("Enter command (e.g., 500,Low): ")
+            if user_input.lower() in ["exit", "quit"]:
+                break
+            start = time.perf_counter()
+            ser.write((user_input + '\n').encode())
+
+except serial.SerialException as e:
+    print(f"Serial error: {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
